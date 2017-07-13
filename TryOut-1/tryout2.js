ae75387dbeb0fd261d242bdf2b881165fa134a58
sasongkoadi <sasongkoadi005@gmail.com>
2017-07-13 18:05:21 +0800

    still not finish

--- TryOut-1/tryout2.js
+++ TryOut-1/tryout2.js
@@ -1,7 +1,10 @@
-function httpGet(theUrl)
+function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
 }
